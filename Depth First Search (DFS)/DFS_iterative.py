96cfbb4ef1d296942779a55af3c00a82f5c724c8
fahim9778 <fahim9778@gmail.com>
2022-04-11 21:36:41 +0600

    Updated codes with class and console input.

--- Depth First Search (DFS)/DFS_iterative.py
+++ Depth First Search (DFS)/DFS_iterative.py
@@ -1,24 +1,73 @@
-def dfs_iterative(start_node, graph):
-    visited = set()
-    if start_node not in graph:
+class Graph:
+    def __init__(self, vertices, is_directed=False):
+        self.nodes = vertices
+        self.adj_list = {}
+        self.is_directed = is_directed
+
+        for node in self.nodes:
+            self.adj_list[node] = set([])
+
+    def add_edge(self, source, destination):
+        self.adj_list[source].add(destination)
+        if not self.is_directed:
+            self.adj_list[destination].add(source)
+
+    def print_adj_list(self):
+        for vertex in self.nodes:
+            print(vertex, ":", self.adj_list[vertex])
+
+
+def dfs_iterative(root_node, graph):
+    """ A function that solves Depth First Search in the Iterative Approach using stack """
+    visited = set()  # to see if the current node is already visited or not
+    if root_node not in graph.adj_list:
         print("Start Node couldn't be found. Program is existing...")
         return
 
-    stack = [start_node]
+    stack = [root_node]
     while stack:
-        node = stack.pop()
-        if node not in visited:
-            print(node)
-            visited.add(node)
-            for neighbor in graph[node]:
-                if neighbor not in visited:
-                    stack.append(neighbor)
+        vertex = stack.pop()
+        if vertex not in visited:
+            print(f'{vertex} -> ', end='')
+            visited.add(vertex)
+            for adjacent_node in graph.adj_list[vertex]:
+                if adjacent_node not in visited:
+                    stack.append(adjacent_node)
+    print('Stop.')
 
 
+"""
 graph = {'A': ['B', 'C', 'D'],
          'B': ['A', 'E', 'D'],
          'C': ['A', 'D'],
          'D': ['A', 'B', 'C', 'E'],
          'E': ['B', 'D']
          }
-dfs_iterative('A', graph)
+"""
+if __name__ == "__main__":
+    number_of_nodes = int(input("Enter How many nodes? "))
+    graph_type = input("D for Directed Graph | U for Undirected Graph : ").lower()
+
+    while True:
+        nodes = list(map(str, input("Enter space separated nodes: ").strip().split()))
+        if len(nodes) != number_of_nodes:
+            print("number of nodes mismatched. Try again ")
+        else:
+            break
+
+    if graph_type == 'd':  # for 'directed' graph
+        graph = Graph(nodes, is_directed=True)
+    else:
+        graph = Graph(nodes, is_directed=False)
+    # print(nodes)
+
+    for node in nodes:
+        neighbor_list = list(map(str, input(f"Enter space separated neighbors for node {node}: ").strip().split()))
+        for neighbor in neighbor_list:
+            graph.add_edge(node, neighbor)
+
+    print(f'Adjacency list is :')
+    graph.print_adj_list()
+
+    start_node = input("Enter root node to start DFS: ")
+    dfs_iterative(start_node, graph)
